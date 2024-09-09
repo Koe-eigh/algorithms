19d99654988985fe026095eb3cb81d704a73b570
Koe-eigh <gongeizhitian@gmail.com>
2024-09-09 16:32:06 +0900

    bubble sort のリファクタリング

--- src/sort/algorithms.py
+++ src/sort/algorithms.py
@@ -9,16 +9,16 @@ def bongo_sort(arr: List[int]) -> List[int]:
         random.shuffle(arr)
     return arr
 
+
+
 def bubble_sort(arr: List[int]) -> List[int]:
-    while 1 == 1:
-        swap_count = 0
-        for i in range(1, len(arr)):
-            if arr[i] < arr[i-1]:
-                arr[i], arr[i-1] = arr[i-1], arr[i]
-                swap_count += 1 
-        if swap_count == 0:
-            break
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+
 
 def selection_sort(arr: List[int]) -> List[int]:
     for i in range(len(arr)):
@@ -31,11 +31,15 @@ def selection_sort(arr: List[int]) -> List[int]:
         arr[i], arr[min_idx] = min, arr[i]
     return arr
 
+
+
 def insertion_sort(arr: List[int]) -> List[int]:
     for i in range(1, len(arr)):
         for j in range(i):
             if arr[i] < arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
+
+
 
 def quick_sort(arr: List[int]) -> List[int]:
     if len(arr) <= 1:
